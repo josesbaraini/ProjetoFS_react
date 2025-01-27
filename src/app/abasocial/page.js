68146877dfef1c9619bc61976025da1b2de40fdb
José Sbaraini <josesbarainips@gmail.com>
2025-01-27 18:48:48 -0400

    ddsd

--- src/app/abasocial/page.js
+++ src/app/abasocial/page.js
@@ -2,10 +2,51 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Meu Titulo</h1>
-  
-      
+    <div className={styles.body}>
+      <main className={styles.mainteste}>
+
+        <div className={styles.container}>
+
+          <div className={styles.lefthalf}>
+            <div className={styles.searchbar}>
+              <div className={styles.searchicon}>🔍</div>
+              <input type="text" placeholder="pesquisar amigo adicionado"></input>
+            </div>
+            <div className={styles.addbutton}>+</div>
+          </div>
+
+
+          <div className={styles.righthalf}>
+            <div className={styles.addfriendbar}>
+              <div className={styles.addicon}>+</div>
+              <span>adicionar amigo</span>
+            </div>
+            <div className={styles.suggestions}>
+              <div className={styles.suggestiontitle}>pessoas que você talvez conheça</div>
+              <div className={styles.suggestioncard}>
+                <a href="tela-chats.html"  className={styles.span}>fulano</a>
+                <div className={styles.usericon}>👤
+                </div>
+              </div>
+              <div className={styles.suggestioncard}>
+                <a href="tela-chats.html"  className={styles.span}>bertano</a>
+                <div className={styles.usericon}>👤
+                </div>
+                </div>
+              <div className={styles.suggestioncard}>
+                <a href="tela-chats.html" className={styles.span}>ciclano</a>
+                <div className={styles.usericon}>👤</div>
+                </div>
+              {/* <div className={styles.suggestioncard highlighted}>
+                <a href="tela-chats.html"  className={styles.span}>vovô chao</a>
+                <div className={styles.usericon}>👤</div>
+                </div> */}
+            </div>
+          </div>
+        </div>
+      </main>
+
+
     </div>
   );
 }
