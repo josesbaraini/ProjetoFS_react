5c04d23eb0d0a39c792a5f8dae8edf2a1a95a3d4
José Sbaraini <josesbarainips@gmail.com>
2025-02-25 13:00:13 -0400

    holy molui

--- src/app/administracao/page.js
+++ src/app/administracao/page.js
@@ -2,11 +2,14 @@
 import styles from "./page.module.css";
 import { useAutenticacao } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const { usuarioId, fazerLogout } = useAutenticacao();
   const roteador = useRouter();
+  const [excluirMaq, setExcluirMaq] = useState(false);
+  const [addMaq, setAddMaq] = useState(false);
+  const [excluirUser, setExcluirUser] = useState(false)
 
   useEffect(() => {
     console.log(usuarioId)
@@ -19,19 +22,58 @@ export default function Home() {
   return (
     <div className={styles.principal}>
       <div className={styles.painel}>
-        <h1>Excluir Conta</h1>
+        <h1>Listar Máquinas</h1>
+        <h1>Listar Ususarios</h1>
         <h1>Adicionar Máquina</h1>
-        <h1>Excluir Máquina</h1>
       </div>
 
-      <div className={styles.excluirMaq}>
+      <table className={styles.tabela}>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Telefone</th>
+                        <th>Email</th>
+                        <th>Cargo</th>
+                        <th>Editar</th>
+                        <th>Deletar</th>
+                    </tr>
+                </thead>
+                <tbody >
+                
+                    <tr>
+                        <td>1</td>
+                        <td>José</td>
+                        <td>99 9999-8990</td>
+                        <td>umemailai@gmail.com</td>
+                        <td>Admin</td>
+                        <td>Editar</td>
+                        <td className={styles.x}>X</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Beatriz</td>
+                        <td>99 9999-8990</td>
+                        <td>outroemailai@gmail.com</td>
+                        <td>Admin</td>
+                        <td>Editar</td>
+                        <td className={styles.x}>X</td>
+                    </tr>
+                    
+
+          
+                </tbody>
+            </table>
+
+      {excluirMaq && <div className={styles.excluirMaq}>
+        <p>Tem certeza que deseja excluir a máquina? Digite o ID da máquina para poder excluir.</p>
         <p>Id Da Máquina</p><input></input>
         <div className={styles.botoes}><button>Excluir</button><button>Cancelar</button></div>
         <p>Excluir uma máquina é uma acão permanente.</p>
 
-      </div>
+      </div>}
 
-      <div className={styles.excluirMaq}>
+      { addMaq && <div className={styles.excluirMaq}>
         <p>Nome da Máquina</p><input></input>
         <div className={styles.botoes}><button>Adicionar</button><button>Cancelar</button></div>
         <p>Adicionar uma máquina para todos os usuarios.</p>
@@ -44,14 +86,15 @@ export default function Home() {
         <p>LicoHipernoitode Superior Direito</p>
 
 
-      </div>
+      </div>}
 
-      <div className={styles.excluirMaq}>
+      { excluirUser && <div className={styles.excluirMaq}>
+      <p>Tem certeza que deseja excluir este usuario? Digite o ID do usuario para poder excluir.</p>
         <p>Id Do Ususario</p><input></input>
         <div className={styles.botoes}><button>Excluir</button><button>Cancelar</button></div>
         <p>Excluir um do banco de dados é uma acão permanente.</p>
 
-      </div>
+      </div>}
       
   
       
